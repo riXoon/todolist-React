f68167fdc0abd9d9ad4632026742889a39be1466
Erickson <rixon.code@gmail.com>
2024-07-30 23:31:23 +0800

    simple UI added

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -5,10 +5,15 @@ export default function TodoInput(props) {
     const { handleAddTodos, todoValue, setTodoValue } = props;
 
     return (
-        <div>
+        <>
+        <div className="topBg"></div>
+        <div className='inputContainer'>
+            <div className="title">
+             <h1>TODO - LIST</h1>
+            </div>
             <header>
                 <input
-                    type="text" 
+                    type="text"
                     value={todoValue}
                     placeholder='Enter to do...'
                     onChange={(e) => setTodoValue(e.target.value)}
@@ -16,9 +21,10 @@ export default function TodoInput(props) {
                 <button className='addBtn' onClick={() => {handleAddTodos(todoValue) 
                     setTodoValue('')
                 }}>
-                    Add
+                    <i class="fa-solid fa-plus icon"></i>
                 </button>
             </header>
         </div>
+        </>
     );
 }
